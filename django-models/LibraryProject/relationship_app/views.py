652154ad740aaf51bfb81343fdf5b2876f8d0f6a
Willow1000 <wilkinsondari7@gmail.com>
2025-02-18 17:21:27 +0300

    made some changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -18,17 +18,17 @@ class LibraryDetailView(DetailView):
     content_type = 'library'
 
 class register(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/templates/relationship_app/register.html'
 
 class LoginView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/templates/relationship_app/login.html'    
 
 class LogoutView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('logout')  
     template_name = 'relationship_app/templates/relationship_app/logout.html'
 
