abd2e63ec18f815be93b670c30999fbd20bf71a1
Willow1000 <wilkinsondari7@gmail.com>
2025-02-18 17:34:52 +0300

    made some changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -32,6 +32,36 @@ class LogoutView(CreateView):
     success_url = reverse_lazy('logout')  
     template_name = 'relationship_app/templates/relationship_app/logout.html'
 
-class AdminView(CreateView):  
-    pass  
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponse
+
+# Function to check if the user is an Admin
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+# Function to check if the user is a Librarian
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+# Function to check if the user is a Member
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Admin view: Accessible only to Admins
+@user_passes_test(is_admin)
+def admin_view(request):
+    return HttpResponse("Welcome, Admin!")
+
+# Librarian view: Accessible only to Librarians
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return HttpResponse("Welcome, Librarian!")
+
+# Member view: Accessible only to Members
+@user_passes_test(is_member)
+def member_view(request):
+    return HttpResponse("Welcome, Member!")
+
       
