e689a4dca7d4537be4b7d0a0a2058b2a41f08c47
Willow1000 <wilkinsondari7@gmail.com>
2025-02-18 11:05:02 +0300

    made some changes

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User as AuthUser
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=255)
@@ -27,3 +27,11 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class User(models.Model):
+    user = models.OneToOneField(AuthUser, on_delete=models.CASCADE)
+    role = models.CharField(max_length=255,choices=['Admin','Librarian','Member'])
+
+    def __str__(self):
+        return self.user.username
