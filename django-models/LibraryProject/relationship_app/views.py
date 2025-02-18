6291dda9525dc1e0ee4da6e72d55b3d598d09e33
Willow1000 <wilkinsondari7@gmail.com>
2025-02-18 17:11:50 +0300

    made some changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -31,4 +31,7 @@ class LogoutView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('logout')  
     template_name = 'relationship_app/logout.html'
+
+class AdminView(CreateView):  
+    pass  
       
