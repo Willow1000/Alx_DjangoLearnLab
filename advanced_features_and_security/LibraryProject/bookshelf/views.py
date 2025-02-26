42a923ad8664861639a55138b78bc5a8931fa0d9
Willow1000 <wilkinsondari7@gmail.com>
2025-02-26 14:41:21 +0300

    forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
-from .forms import ExampleForm
-# Create your views here.
+from django.http import HttpResponseForbidden
+from django.db.models import Q
+from .models import Book
+from .forms import ExampleForm, BookForm
 
-@permission_required('bookshelf.can_add_book',raise_exception=True)
+# View to add a new book (requires 'can_add_book' permission)
+@permission_required('bookshelf.can_add_book', raise_exception=True)
 def add_book(request):
-    return render(request, "add_book.html")
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "bookshelf/book_list.html", {"message": "Book added successfully!"})
+    else:
+        form = BookForm()
+    
+    return render(request, "bookshelf/add_book.html", {"form": form})
 
-
-book_list
+# View to display the book list (requires 'can_view_book' permission)
+@permission_required('bookshelf.can_view_book', raise_exception=True)
+def book_list(request):
+    query = request.GET.get("q", "")
+    
+    if query:
+        books = Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query))
+    else:
+        books = Book.objects.all()
+    
+    return render(request, "bookshelf/book_list.html", {"books": books})
