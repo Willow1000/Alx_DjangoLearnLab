8e82d433ba3160b1d028c17f8ad1b2679fea08e1
Willow1000 <wilkinsondari7@gmail.com>
2025-02-26 13:35:01 +0300

    made some changes

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
+
+@permission_required('bookshelf.can_add_book',raise_exception=True)
+def add_book(request):
+    return render(request, "add_book.html")
