8ee8da53376794de01e9332269d918fba7c22eda
Willow1000 <wilkinsondari7@gmail.com>
2025-03-03 17:44:46 +0300

    task 1 commit 1

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import rest_framework
+from .serializers import BookSerializer
+from .models import Book
 
 # Create your views here.
+class BookList(rest_framework.generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
