0596b7142762d9e143aec7f25102a0e7c3b43768
Willow1000 <wilkinsondari7@gmail.com>
2025-03-11 13:32:19 +0300

    created customised generic views and updated urls.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializers import BookSerializer
+# from rest_framework.mixins 
+
 
 # Create your views here.
+# Retrieves all books
+class ListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Retrieve specific book using ID
+class DetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class =  BookSerializer
+
+# creates a new book
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Updates record of existing book
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
