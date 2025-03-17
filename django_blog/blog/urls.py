61642a02cb29101bc193c7c6f48dddbbaf3e2865
Willow1000 <wilkinsondari7@gmail.com>
2025-03-17 18:14:31 +0300

    made some changes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
     path("update/<int:pk>/",UpdateblogView.as_view(),name="update"),
     path("post/new/",CreateComment.as_view(),name="comment"),
     path("blog/<int:pk>/comments",Comments.as_view(),name="comments")
+    path("comment/<int:pk>/update/",Comments.as_view(),name="comments")
+    path("post/<int:pk>/comments/new/",Comments.as_view(),name="comments")
+    path("comment/<int:pk>/delete/",Comments.as_view(),name="comments")
 ]
 
 from Django_Blog import settings
