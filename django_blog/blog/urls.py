0cae9907c0aa1e368a2e735277763da2cf936986
Willow1000 <wilkinsondari7@gmail.com>
2025-03-17 17:29:26 +0300

    made some changes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path("",HomeView.as_view(),name="home"),
-    path("signup/",RegistrationView.as_view(),name='registration'),
+    path("/profile",HomeView.as_view(),name="home"),
+    path("register/",RegistrationView.as_view(),name='registration'),
     path('login/',CustomLoginView.as_view(),name='login'),
     path('logout/',CustomLogoutView.as_view(next_page='login'),name='logout'),
     path("createblog",CreateBlogView.as_view(),name="createblog"),
